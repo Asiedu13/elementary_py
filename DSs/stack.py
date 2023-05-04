86ae1512004c628d9ab58618a72780c002a2c009
prince <princekofasiedu@gmail.com>
2023-05-04 13:08:49 +0000

    Reversed a word with a stack"

--- DSs/stack.py
+++ DSs/stack.py
@@ -2,12 +2,31 @@ class Stack():
     def __init__(self):
         self.arr = []
     
+    def populate(self, word):
+        self.arr = [i for i in word]
+    
+    
     def push(self, value):
         self.arr.append(value)
+    
+        
     def pop(self):
         return self.arr.pop()
+    
+    
     def read(self):
         print([i for i in self.arr])
+    
+    
+    def reverse_content(self):
+        reversed_content = ''
+        arrCopy = self.arr.copy()
+        
+        for i in self.arr:
+            reversed_content += arrCopy.pop()
+            
+        return str(reversed_content)
+    
     
     def __str__(self):
         return "This is the Stack parent class"
@@ -18,3 +37,12 @@ print(type(parting_list))
 parting_list.push('books')
 parting_list.push('bread')
 parting_list.read()
+
+print(parting_list)
+
+
+# Using a stack to reverse a string
+word = Stack()
+word.populate('abcde')
+word.read()
+print(word.reverse_content())
